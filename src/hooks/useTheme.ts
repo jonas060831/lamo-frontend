25fdb3bdeb68eefa07365070d29d522188325cdb
jonaspaulosulit <jonas_sulit2001@yahoo.com>
2026-03-11 01:14:21 -0700

    added light and dark mode using useTheme hook

---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export default function useTheme() {
+  const [theme, setTheme] = useState<"light" | "dark">(() => {
+    const savedTheme = localStorage.getItem("theme") as "light" | "dark" | null;
+
+    if (savedTheme) return savedTheme;
+
+    // fallback to system preference
+    return window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+  });
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+
+    // remember user preference
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () =>
+    setTheme(prev => (prev === "light" ? "dark" : "light"));
+
+  return { theme, toggleTheme };
+}
--- src/shared/navbar/Navbar.tsx
+++ src/shared/navbar/Navbar.tsx
@@ -1,9 +1,96 @@
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import styles from "./Navbar.module.css";
+
+import Menu from '../../assets/svgs/menu.svg'
+import arrowRight from '../../assets/svgs/arrowRight.svg'
+import lightIcon from '../../assets/svgs/sun.svg'
+import darkIcon from '../../assets/svgs/moon.svg'
+
+import { Link } from "react-router";
+import useTheme from "../../hooks/useTheme";
+export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  const { theme, toggleTheme } = useTheme()
 
 
-const Navbar = () => {
+  const handleDismiss = () => setOpen(false)
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <nav className={styles.navbar}>
+        <button
+          className={styles.menuButton}
+          onClick={() => setOpen(true)}
+          aria-label="menu"
+        >
+          <img 
+           className={`svg ${styles.menuImg}`}
+           src={Menu}
+          />
+        </button>
+      </nav>
 
-export default Navbar
+      <AnimatePresence>
+        {open && (
+          <>
+            {/* Backdrop */}
+            <motion.div
+              className={styles.backdrop}
+              onClick={() => setOpen(false)}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            />
+
+            {/* Drawer */}
+            <motion.div
+              className={styles.drawer}
+              initial={{ x: "100%" }}
+              animate={{ x: 0 }}
+              exit={{ x: "100%" }}
+              transition={{
+                type: "spring",
+                stiffness: 260,
+                damping: 25
+              }}
+            >
+              <button
+                className={styles.arrowRight}
+                onClick={() => setOpen(false)}
+              >
+                <img 
+                 className={`svg ${styles.arrowRightImg}`}
+                 src={arrowRight}
+                />
+              </button>
+
+              <ul className={styles.links}>
+                <li onClick={handleDismiss}><Link to="/">Home</Link></li>
+                <li>
+                    <label className={styles.themeToggle}>
+                      <input
+                        type="checkbox"
+                        checked={theme === "dark"}
+                        onChange={toggleTheme}
+                      />
+
+                      <span className={styles.slider}>
+                        <img
+                          src={theme === "dark" ? darkIcon : lightIcon}
+                          className={styles.icon}
+                          alt="theme icon"
+                        />
+                      </span>
+                    </label>
+                  </li>
+
+              </ul>
+            </motion.div>
+          </>
+        )}
+      </AnimatePresence>
+    </>
+  );
+}
