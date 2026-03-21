8c380066978667a181420813f1c7e82ac9549de7
jonaspaulosulit <jonas_sulit2001@yahoo.com>
2026-03-20 20:08:47 -0700

    remove setTimeout hack which iOs blocks and use useRef instead fix

--- src/hooks/useSpeechRecognition.ts
+++ src/hooks/useSpeechRecognition.ts
@@ -16,6 +16,8 @@ const useSpeechRecognition = () => {
 
   const hasRecognitionSupport = !!SpeechRecognition
 
+  const isUserActivatedRef = useRef(false)
+
   useEffect(() => {
     if (!SpeechRecognition) return
 
@@ -40,8 +42,20 @@ const useSpeechRecognition = () => {
     }
 
     recognition.onend = () => {
-      if (shouldRestartRef.current) {
-        setTimeout(() => recognition.start(), 200)
+      // if (shouldRestartRef.current) {
+      //   setTimeout(() => recognition.start(), 200)
+      // } else {
+      //   setIsListening(false)
+      // }
+
+      if(shouldRestartRef.current && isUserActivatedRef.current) {
+        try {
+          recognition.start()
+
+        } catch (error) {
+          console.log('Restart blocked:', error)
+          setIsListening(false)
+        }
       } else {
         setIsListening(false)
       }
@@ -49,6 +63,12 @@ const useSpeechRecognition = () => {
     recognition.onerror = async (event: any) => {
 
       console.log("Speech Error:", event.error)
+
+      if(event.error === "not-allowed") {
+        isUserActivatedRef.current = false
+        shouldRestartRef.current = false
+      }
+
       setIsListening(false)
     }
 
@@ -58,11 +78,26 @@ const useSpeechRecognition = () => {
     const recognition = recognitionRef.current
     if (!recognition) return
 
+    // shouldRestartRef.current = true
+    // setIsListening("starting")
+    // setTimeout(() => {
+    //     recognition.start()
+    // },1000)
+
+    isUserActivatedRef.current = true
+
     shouldRestartRef.current = true
+
     setIsListening("starting")
-    setTimeout(() => {
-        recognition.start()
-    },1000)
+
+    recognition.start()
+
+    try {
+      
+    } catch (error) {
+      console.log('Start error', error)
+      setIsListening(false)
+    }
   }
 
   const stopListening = () => {
@@ -70,12 +105,14 @@ const useSpeechRecognition = () => {
     if (!recognition) return
 
     shouldRestartRef.current = false
+    isUserActivatedRef.current = false
     
-    setIsListening("starting")
-    setTimeout(() => {
-        setIsListening(false)
-        recognition.stop()
-    },1000)
+    try {
+      recognition.stop()
+
+    } catch (error) {
+      console.log("Stop error: ", error)
+    }
   }
 
   const requestPermission = () => {
